be2caca4737c2b1d6a31015e99553ec46f33076a
Matthew Thomas <mt528@canterbury.ac.uk>
2021-03-10 20:14:17 +0000

    Added a Katakana class - Not done anything with it for now. and updated the program.cs

---
+++ JapaneseKatakana.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Language_Learning_Project
+{
+    class JapaneseKatakana
+    {
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,11 +6,17 @@ namespace Language_Learning_Project
     {
         static void Main(string[] args)
         {
-            // IntroCode.Intro(); // Looks like its working 
 
-           // MainMenu.MainMenuFunction(); // Runs the MainMenuFunction within the MainMenu Class 
+
+            // IntroCode.Intro(); // Looks like its working
+
+            // MainMenu.MainMenuFunction(); // Runs the MainMenuFunction within the MainMenu Class 
 
             JapaneseHiragana.Menu(); // FOR TESTING
+
+            //JapaneseHiragana.Class1(); // Testing
+
+            //JapaneseHiragana.Class1Test();
             
         }
     }
